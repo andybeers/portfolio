5e9e86bac99c06f7bc6dd49065d6046c0079b1c3
Andrew Beers <andybeers@gmail.com>
2016-09-20 21:02:38 -0700

    Added portfolio item contructor to portfolio javascript file. Use jQuery to populate template with portfolioItems and append to the dom.

---
+++ scripts/portfolio.js
@@ -0,0 +1,44 @@
+var portfolioItems = [];
+
+function Portfolio (opts) {
+  this.title = opts.title;
+  this.category = opts.category;
+  this.previewUrl = opts.previewUrl;
+  this.previewAlt = opts.previewAlt;
+  this.linkUrl = opts.linkUrl;
+  this.description = opts.description;
+  this.postedOn = opts.postedOn;
+}
+
+// var firstArticle = ourLocalData[0];
+// new Article(firstArticle);
+
+Portfolio.prototype.toHtml = function() {
+  var $newPortfolio = $('.template').clone();
+
+  $newPortfolio.attr('data-category', this.category);
+  $newPortfolio.find('.title').text(this.title);
+  $newPortfolio.find('.preview img').attr('src', this.previewUrl);
+  $newPortfolio.find('.preview img').attr('alt', this.previewAlt);
+  $newPortfolio.find('.description').html(this.description);
+  $newPortfolio.find('.preview a').attr('href', this.linkUrl);
+  $newPortfolio.find('time[pubdate]').attr('title', this.postedOn);
+  $newPortfolio.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+
+  $newPortfolio.removeClass('template');
+
+  return $newPortfolio;
+
+};
+
+portfolioData.sort(function(curElem, nextElem) {
+  return (new Date(nextElem.publishedOn)) - (new Date(curElem.publishedOn));
+});
+
+portfolioData.forEach(function(ele) {
+  portfolioItems.push(new Portfolio(ele));
+});
+
+portfolioItems.forEach(function(a) {
+  $('#portfolio').append(a.toHtml());
+});
--- scripts/portfolioItems.js
+++ scripts/portfolioItems.js
@@ -2,29 +2,37 @@ var portfolioData = [
 
   {
     title: 'coffeeBuddy',
+    category: 'web-app',
     previewUrl: 'img/coffeeBuddy.png',
     previewAlt: 'Screenshot of coffeeBuddy web application',
+    linkUrl: 'https://andybeers.github.io/coffeeBuddy/',
     description: 'A simple web app to calculate ideal coffee brew recipes',
     postedOn: '2016-09-20',
   },
   {
     title: '#',
+    category: 'web-app',
     previewUrl: 'img/placeholder.png',
     previewAlt: 'Placeholder image',
+    linkUrl: '#',
     description: 'I am a description placeholder.',
     publishedOn: '2016-09-20',
   },
   {
     title: '#',
+    category: '#',
     previewUrl: 'img/placeholder.png',
     previewAlt: 'Placeholder image',
+    linkUrl: '#',
     description: 'I am a description placeholder.',
     publishedOn: '2016-09-20',
   },
   {
     title: '#',
+    category: '#',
     previewUrl: 'img/placeholder.png',
     previewAlt: 'Placeholder image',
+    linkUrl: '#',
     description: 'I am a description placeholder.',
     publishedOn: '2016-09-20',
   }
