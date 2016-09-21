f439b48b5c9f1430b0dfc3ff67f29e7c97550715
Andrew Beers <andybeers@gmail.com>
2016-09-20 20:34:15 -0700

    Added data array objects to data js file.

---
+++ scripts/portfolioItems.js
@@ -0,0 +1,32 @@
+var portfolioData = [
+
+  {
+    title: 'coffeeBuddy',
+    previewUrl: 'img/coffeeBuddy.png',
+    previewAlt: 'Screenshot of coffeeBuddy web application',
+    description: 'A simple web app to calculate ideal coffee brew recipes',
+    postedOn: '2016-09-20',
+  },
+  {
+    title: '#',
+    previewUrl: 'img/placeholder.png',
+    previewAlt: 'Placeholder image',
+    description: 'I am a description placeholder.',
+    publishedOn: '2016-09-20',
+  },
+  {
+    title: '#',
+    previewUrl: 'img/placeholder.png',
+    previewAlt: 'Placeholder image',
+    description: 'I am a description placeholder.',
+    publishedOn: '2016-09-20',
+  },
+  {
+    title: '#',
+    previewUrl: 'img/placeholder.png',
+    previewAlt: 'Placeholder image',
+    description: 'I am a description placeholder.',
+    publishedOn: '2016-09-20',
+  }
+
+];
