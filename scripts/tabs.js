9bb43ce8e29fcbded3efbedec86048db9fe640b0
Andrew Beers <andybeers@gmail.com>
2016-09-25 20:17:23 -0400

    Refactored tab functionality code. Instead of iterating through every item's data category, we select one directly.

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -3,15 +3,9 @@ var tabs = {};
 tabs.handleMainNav = function () {
   $('.main-nav .tab').on('click', function() {
     $('.tab-content').hide();
-    if ($(this).attr('data-category') === 'portfolio') {
-      $('#portfolio').fadeIn();
-    } else if ($(this).attr('data-category') === 'about') {
-      $('#about').fadeIn();
-    }
+    $('#' + $(this).data('category')).fadeIn();
   });
-
   $('.main-nav .tab:first').click();
-  
 };
 
 tabs.handleMainNav();
